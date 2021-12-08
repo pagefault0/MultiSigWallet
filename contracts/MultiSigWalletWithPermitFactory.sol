29fcc02da1876b9af07adf9ef95f50428fca821b
pagefault0 <pagefault@126.com>
2021-12-08 16:18:45 +0800

    Create MultiSigWalletWithPermitFactory.sol

---
+++ contracts/MultiSigWalletWithPermitFactory.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: lgplv3
+pragma solidity ^0.8.0;
+
+import "./Factory.sol";
+import "./MultiSigWalletWithPermit.sol";
+
+
+/// @title Multisignature wallet factory - Allows creation of multisig wallet.
+/// @author Stefan George - <stefan.george@consensys.net>
+contract MultiSigWalletWithPermitFactory is Factory {
+
+    /*
+     * Public functions
+     */
+    /// @dev Allows verified creation of multisignature wallet.
+    /// @param _owners List of initial owners.
+    /// @param _required Number of required confirmations.
+    /// @return wallet Returns wallet address.
+    function create(address[] memory _owners, uint _required)
+        public
+        returns (address wallet)
+    {
+        wallet = (address)(new MultiSigWalletWithPermit(_owners, _required));
+        register(wallet);
+    }
+}
