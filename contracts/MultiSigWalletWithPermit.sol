8f67302e3694a7a8fdf1d3bd9a13f385587dd7c9
pagefault0 <pagefault@126.com>
2021-12-22 10:08:50 +0800

    Update MultiSigWalletWithPermit.sol

--- contracts/MultiSigWalletWithPermit.sol
+++ contracts/MultiSigWalletWithPermit.sol
@@ -66,60 +66,37 @@ contract MultiSigWalletWithPermit is MultiSigWallet {
     }
 
     bytes32 public DOMAIN_SEPARATOR;
-    bytes32 public constant PERMIT_TYPEHASH = 0x8d14977a529be0cde9be2de41261d56c536e10c2bfb3f797a663ac4f3676d2fe;
+    bytes32 public constant PERMIT_TYPEHASH =
+        0x8d14977a529be0cde9be2de41261d56c536e10c2bfb3f797a663ac4f3676d2fe;
 
     /*
-     * delegateCallWithPermit
+     * executeTxWithPermits
      */
     /// @dev delegate call
     /// @param destination Transaction target address.
     /// @param value Transaction ether value.
     /// @param data Transaction data payload.
-    /// @param transactionId Transaction ID.
+    /// @param nonce Transaction ID.
     /// @return newTransactionId Returns transaction ID.
-    function delegateCallWithPermits(
+    function executeTxWithPermits(
         address destination,
         uint256 value,
         bytes memory data,
-        uint256 transactionId,
+        uint256 nonce,
         bytes32[] memory rs,
         bytes32[] memory ss,
         uint8[] memory vs
-    ) public returns (uint256[] memory newTransactionId) {
+    ) public returns (uint256 newTransactionId) {
         require(rs.length == ss.length, "invalid signs");
         require(rs.length == vs.length, "invalid signs2");
-        newTransactionId = new uint256[](rs.length);
-        for (uint8 i = 0; i < rs.length; ++i) {
-            newTransactionId[i] = delegateCallWithPermit(
-                destination,
-                value,
-                data,
-                transactionId,
-                rs[i],
-                ss[i],
-                vs[i]
-            );
-        }
-    }
+        require(
+            nonce == transactionCount,
+            "invalid transactionId(nonce)"
+        );
+        require(rs.length == required, "invalid signs3");
+        require(destination != address(0), "invalid destination");
 
-    /*
-     * delegateCallWithPermit
-     */
-    /// @dev delegate call
-    /// @param destination Transaction target address.
-    /// @param value Transaction ether value.
-    /// @param data Transaction data payload.
-    /// @param transactionId Transaction ID.
-    /// @return newTransactionId Returns transaction ID.
-    function delegateCallWithPermit(
-        address destination,
-        uint256 value,
-        bytes memory data,
-        uint256 transactionId,
-        bytes32 r,
-        bytes32 s,
-        uint8 v
-    ) public returns (uint256 newTransactionId) {
+        newTransactionId = addTransaction(destination, value, data);
         bytes32 digest = keccak256(
             abi.encodePacked(
                 "\x19\x01",
@@ -131,24 +108,25 @@ contract MultiSigWalletWithPermit is MultiSigWallet {
                         destination,
                         value,
                         keccak256(data),
-                        transactionId
+                        nonce
                     )
                 )
             )
         );
 
-        address owner = ecrecover(digest, v, r, s);
-        require(owner != address(0), "0 address");
-        require(isOwner[owner]);
+        for (uint8 i = 0; i < rs.length; ++i) {
+            address owner = ecrecover(digest, vs[i], rs[i], ss[i]);
+            require(owner != address(0), "0 address");
+            require(isOwner[owner], "not owner");
 
-        if (destination != address(0)) {
-            require(transactionId == transactionCount, "invalid transactionId(nonce)");
-            newTransactionId = addTransaction(destination, value, data);
-            confirmTransactionInner(transactionId, owner);
-        } else {
-            confirmTransactionInner(transactionId, owner);
-            newTransactionId = transactionId;
+            confirmTransactionInner(newTransactionId, owner);
         }
+
+        if (isConfirmed(newTransactionId)) {
+            executeTransactionInner(newTransactionId);
+        }
+
+        require(transactions[newTransactionId].executed, "tx failed");
     }
 
     function confirmTransactionInner(uint256 transactionId, address owner)
@@ -158,28 +136,20 @@ contract MultiSigWalletWithPermit is MultiSigWallet {
     {
         confirmations[transactionId][owner] = true;
         emit Confirmation(owner, transactionId);
-        executeTransactionInner(transactionId);
     }
 
     function executeTransactionInner(uint256 transactionId)
         private
         notExecuted(transactionId)
     {
-        if (isConfirmed(transactionId)) {
-            Transaction storage txn = transactions[transactionId];
-            txn.executed = true;
-            if (
-                external_call(
-                    txn.destination,
-                    txn.value,
-                    txn.data.length,
-                    txn.data
-                )
-            ) emit Execution(transactionId);
-            else {
-                emit ExecutionFailure(transactionId);
-                txn.executed = false;
-            }
+        Transaction storage txn = transactions[transactionId];
+        txn.executed = true;
+        if (
+            external_call(txn.destination, txn.value, txn.data.length, txn.data)
+        ) emit Execution(transactionId);
+        else {
+            emit ExecutionFailure(transactionId);
+            txn.executed = false;
         }
     }
 }
